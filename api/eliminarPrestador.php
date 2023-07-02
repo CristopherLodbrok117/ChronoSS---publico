10fd65af155e39dd35a6ed521c7696e99a0cb267
Arturo Sahagun <arturopbsahagun@gmail.com>
2023-07-01 23:58:35 -0600

    modificados los scripts para dar de alta y eliminar

--- api/eliminarPrestador.php
+++ api/eliminarPrestador.php
@@ -1,7 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
-  
+
+require "_auth.php";
+
 $servername = "db5013554698.hosting-data.io";
 $username = "dbu1229465";
 $password = "cuceimobile";
@@ -11,16 +13,22 @@ $conn = new mysqli("localhost", "arturo", "cuceimobile", "chronoss_php");
 
 
 try{
-    //$idProfe = $_POST["idprofe"];
     $codigo = $_POST["codigo"];
+    $token = $_POST["token"];
+    
+    $idProfe = getProfe($conn, $codigo);
+    
+    if(autenticarToken($conn, "profe", $token, $idProfe) == 1){
+        $query = sprintf("delete from timelog where prestador = %s", $codigo);
+        $result = $conn->query($query);
 
-    $query = sprintf("delete from timelog where prestador = %s", $codigo);
-    $result = $conn->query($query);
+        $query = sprintf("delete from prestador where idPrestador = %s", $codigo);
+        $result2 = $conn->query($query);
 
-    $query = sprintf("delete from prestador where idPrestador = %s", $codigo);
-    $result2 = $conn->query($query);
-
-    echo $result.$result2;
+        echo "Prestador eliminado";
+    }else{
+        echo "X";
+    }
 }
 
 catch(Exception $e) {echo 'Message: ' .$e->getMessage();}
--- api/getAllPrestadorInfo.php
+++ api/getAllPrestadorInfo.php
@@ -1,7 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, OPTIONS, POST");
-    
+
+require "_auth.php";
+
 $servername = "db5013554698.hosting-data.io";
 $username = "dbu1229465";
 $password = "cuceimobile";
@@ -11,33 +13,37 @@ $conn = new mysqli("localhost", "arturo", "cuceimobile", "chronoss_php");
 
 
 try{
-    $idProfe = 777;
-    $query = sprintf("select idPrestador, nombre, enServicio, tiempoAcumulado from prestador where jefe = %s", $idProfe);
-    $result = $conn->query($query);
+    $idProfe = $_GET["idprofe"];
+    $token = $_GET["token"];
+    if(autenticarToken($conn, "profe", $token, $idProfe) == 1){
+        $query = sprintf("select idPrestador, nombre, enServicio, tiempoAcumulado from prestador where jefe = %s", $idProfe);
+        $result = $conn->query($query);
 
-    $senddata = array();
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $timelogs = array();
-            $res = $conn->query("select tipo, stamp from timelog where prestador = ".$row["idPrestador"].";");
-            if($res->num_rows > 0){
-                while($r = $res->fetch_assoc()){
-                    $a = array("stamp" => $r["stamp"], "tipo" => $r["tipo"]);
-                    array_push($timelogs, $a);
+        $senddata = array();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $timelogs = array();
+                $res = $conn->query("select tipo, stamp from timelog where prestador = ".$row["idPrestador"].";");
+                if($res->num_rows > 0){
+                    while($r = $res->fetch_assoc()){
+                        $a = array("stamp" => $r["stamp"], "tipo" => $r["tipo"]);
+                        array_push($timelogs, $a);
+                    }
                 }
+                $arr = array("codigo" => $row["idPrestador"],
+                            "nombre" => $row["nombre"],
+                            "enServicio" => $row["enServicio"],
+                            "tiempoAcumulado" => $row["tiempoAcumulado"],
+                            
+                            "timelogs" => $timelogs
+                            );
+                array_push($senddata, $arr);
             }
-            $arr = array("codigo" => $row["idPrestador"],
-                        "nombre" => $row["nombre"],
-                        "enServicio" => $row["enServicio"],
-                        "tiempoAcumulado" => $row["tiempoAcumulado"],
-                        
-                        "timelogs" => $timelogs
-                        );
-            array_push($senddata, $arr);
         }
+        echo json_encode($senddata);
+    }else{
+        echo "X";
     }
-    echo json_encode($senddata);
-
 }
 
 catch(Exception $e) {echo 'Message: ' .$e->getMessage();}
