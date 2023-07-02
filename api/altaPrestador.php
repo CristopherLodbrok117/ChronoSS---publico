0e4bd2defa83c3f36fd2896f0cb7e289ed726aca
Arturo Sahagun <arturopbsahagun@gmail.com>
2023-07-01 21:22:39 -0600

    fixed typo

--- api/altaPrestador.php
+++ api/altaPrestador.php
@@ -1,14 +1,15 @@
 <?php
-require "_auth.php";
-
-$servername = "db5013554698.hosting-data.io";
-$username = "dbu1229465";
-$password = "cuceimobile";
-$dbname = "dbs11355777";
-$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 try{
+    require "_auth.php";
+
+    $servername = "db5013554698.hosting-data.io";
+    $username = "dbu1229465";
+    $password = "cuceimobile";
+    $dbname = "dbs11355777";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     $idProfe = $_POST["idprofe"];
     $codigo = $_POST["codigo"];
     $nombre = $_POST["nombre"];
