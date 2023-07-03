3de0fa4bd941d9453661cf64c41ee5bffcdcfebd
Cristopher <cristopheralexis.chavez@alumnos.udg.mx>
2023-07-03 16:37:13 -0600

    Adaptando script-password de vistas/nuevoPanel a nuevos cambios en la api

--- vistas/nuevoPanel/script-password.js
+++ vistas/nuevoPanel/script-password.js
@@ -1,9 +1,7 @@
-
-
 // const backendServer = "http://148.202.152.33/Servicio_Social";
 const backendServer = "https://chronoss.mx/api";
-const servicio = "/altaPrestador.php";
-const tipoRequest = "POST"; 
+
+const RESPUESTA_SERVER_ERRONEA = "X";
 
 const BG_ACTUALIZADO = "password-actualizada";
 const BG_ERROR = "password-erronea";
@@ -15,46 +13,49 @@ document.querySelector("#boton-cambiar").addEventListener("click", function(){
   let nuevacontra2 = document.querySelector("#new-password-rep").value;
   
   // Pruebas
-  if(viejacontra == 'mal') {  // Eliminar if
-    mostrarToast(TITULO_WARNING, BACKGROUND_WARNING, "Contraseña actual mal escrita", ICONO_WARNING);
-    actualizarFondoImg(BG_ERROR);
-    return;
-  }
-  else if(nuevacontra != nuevacontra2){
+  // if(viejacontra == 'mal') {  // Eliminar if
+  //   mostrarToast(TITULO_WARNING, BACKGROUND_WARNING, "Contraseña actual mal escrita", ICONO_WARNING);
+  //   actualizarFondoImg(BG_ERROR);
+  //   return;
+  // }
+  if(nuevacontra != nuevacontra2){
       mostrarToast(TITULO_WARNING, BACKGROUND_WARNING, "Las nuevas contraseñas no coinciden", ICONO_WARNING)
       actualizarFondoImg(BG_WARNING);
       return;
   }
-  else { // Eliminar if
-    mostrarToast(TITULO_SUCCESS, BACKGROUND_ESPERA, "Se actualizo la contraseña", ICONO_SUCCESS);
-    actualizarFondoImg(BG_ACTUALIZADO);
-    return;
-  }
+  // else { // Eliminar if
+  //   mostrarToast(TITULO_SUCCESS, BACKGROUND_ESPERA, "Se actualizo la contraseña", ICONO_SUCCESS);
+  //   actualizarFondoImg(BG_ACTUALIZADO);
+  //   return;
+  // }
 
 
-  // const senddata = new FormData();
-  // senddata.append("contra", viejacontra);
-  // senddata.append("contranueva", nuevacontra);
+  const senddata = new FormData();
+  senddata.append("vieja", viejacontra);
+  senddata.append("nueva", nuevacontra);
+  senddata.append("idprofe", localStorage.getItem('idprofe'));
 
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //     if (this.readyState == 4 && this.status == 200) {
-  //         let respuesta = xhttp.responseText;
-  //         if(respuesta == "-1"){
-  //             mostrarToast(TITULO_WARNING, BACKGROUND_WARNING, "Tu actual contraseña no coincide", ICONO_WARNING);
-  //             actualizarFondoImg(BG_ERROR);
-  //             location.reload();
-  //         }else{
-  //             mostrarToast(TITULO_SUCCESS, BACKGROUND_ESPERA, "Se actualizo la contraseña", ICONO_SUCCESS);
-  //             actualizarFondoImg(BG_ACTUALIZADO);
-  //             limpiarCampos();
-  //             //location.reload();
-  //         }
-  //     }
-  // };
-  // xhttp.open("POST", `${backendServer}/cambiarContra.php`, true);
-  // xhttp.send(senddata);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          let respuesta = xhttp.responseText;
+          if(respuesta == RESPUESTA_SERVER_ERRONEA){
+              mostrarToast(TITULO_WARNING, BACKGROUND_WARNING, "Tu actual contraseña no coincide", ICONO_WARNING);
+              actualizarFondoImg(BG_ERROR);
+              // location.reload();
+          }else{
+              mostrarToast(TITULO_SUCCESS, BACKGROUND_ESPERA, "Se actualizo la contraseña", ICONO_SUCCESS);
+              actualizarFondoImg(BG_ACTUALIZADO);
+              limpiarCampos();
+              //location.reload();
+          }
+      }
+  };
+  xhttp.open("POST", `${backendServer}/cambiarContra.php`, true);
+  xhttp.send(senddata);
 });
+
+// Animaciones y mas features de la página
 
 function actualizarFondoImg(claseColorBg) {
   const imgBackground = document.querySelector("#img-bg");
