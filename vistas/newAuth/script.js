a748a6f1002a559ee2da6b11d1b5ae12815f066d
Cristopher <cristopheralexis.chavez@alumnos.udg.mx>
2023-07-03 16:23:18 -0600

    Adaptando script de vistas/newAuth a nuevos cambios en la api

--- vistas/newAuth/script.js
+++ vistas/newAuth/script.js
@@ -1,11 +1,9 @@
-//const backendServer = "http://127.0.0.1:5000";
-//const backendServer = "http://148.202.152.33/Servicio_Social";
 const backendServer = "https://chronoss.mx/api";
 
 
-document.querySelector("#autorizar-button").addEventListener("click", function(){
-    let idProfe = document.querySelector("#idprofe-input").value;
-    let givenPassword = document.querySelector("#password-input").value;
+document.querySelector("#boton-autorizar").addEventListener("click", function(){ 
+    let idProfe = document.querySelector("#id-profe").value;
+    let givenPassword = document.querySelector("#password-profe").value;
     let tipo = document.querySelector("#tipo-combo").value;
 
 
@@ -27,39 +25,6 @@ document.querySelector("#autorizar-button").addEventListener("click", function()
     };
     xhttp.open("GET", `${backendServer}/grantToken.php?idprofe=${idProfe}&password=${givenPassword}&tipo=${tipo}`, true);
     xhttp.send(null);
-});
-
-
-
-document.querySelector("#boton-cambiar").addEventListener("click", function(){
-    let viejacontra = document.querySelector("#old-pw").value;
-    let nuevacontra = document.querySelector("#new-pw").value;
-    let nuevacontra2 = document.querySelector("#new-pw-rep").value;
-    
-    if(nuevacontra != nuevacontra2){
-        alert("La nueva contraseñas no coincide!");
-        return;
-    }
-
-    const senddata = new FormData();
-    senddata.append("contra", viejacontra);
-    senddata.append("contranueva", nuevacontra);
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let respuesta = xhttp.responseText;
-            if(respuesta == "-1"){
-                alert("Contraseña vieja erronea");
-                location.reload();
-            }else{
-                alert("La contraseña se cambió!");
-                location.reload();
-            }
-        }
-    };
-    xhttp.open("POST", `${backendServer}/cambiarContra.php`, true);
-    xhttp.send(senddata);
 });
 
 
